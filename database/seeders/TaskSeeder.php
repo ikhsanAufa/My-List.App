1cc9d621c7f5731965e7bdf56058dea1047162c8
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-09 22:38:50 +0700

    buat schema task seeder

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
+use App\Models\TaskList;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,58 @@ class TaskSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $tasks = [
+            [
+                'name' => 'Belajar Laravel',
+                'description' => 'Belajar Laravel di santri koding',
+                'is_completed' => false,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'Belajar')->first()->id,
+            ],
+            [
+                'name' => 'Belajar React',
+                'description' => 'Belajar React di WPU',
+                'is_completed' => true,
+                'priority' => 'high',
+                'list_id' => TaskList::where('name', 'Belajar')->first()->id,
+            ],
+            [
+                'name' => 'Pantai',
+                'description' => 'Liburan ke Pantai bersama keluarga',
+                'is_completed' => false,
+                'priority' => 'low',
+                'list_id' => TaskList::where('name', 'Liburan')->first()->id,
+            ],
+            [
+                'name' => 'Villa',
+                'description' => 'Liburan ke Villa bersama teman sekolah',
+                'is_completed' => true,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'Liburan')->first()->id,
+            ],
+            [
+                'name' => 'Matematika',
+                'description' => 'Tugas Matematika bu Nina',
+                'is_completed' => true,
+                'priority' => 'medium',
+                'list_id' => TaskList::where('name', 'Tugas')->first()->id,
+            ],
+            [
+                'name' => 'PAIBP',
+                'description' => 'Tugas presentasi pa budi',
+                'is_completed' => false,
+                'priority' => 'high',
+                'list_id' => TaskList::where('name', 'Tugas')->first()->id,
+            ],
+            [
+                'name' => 'Project Ujikom',
+                'description' => 'Membuat project Todo App untuk ujikom',
+                'is_completed' => false,
+                'priority' => 'high',
+                'list_id' => TaskList::where('name', 'Tugas')->first()->id,
+            ],
+        ];
+
+        Task::insert($tasks);
     }
 }
