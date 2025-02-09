d8b54a5c1cb7435e0e00faa5cbf2f02e1189c8e1
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-09 22:34:36 +0700

    buat schema table Task

--- database/migrations/2025_02_09_152830_create_tasks_table.php
+++ database/migrations/2025_02_09_152830_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->boolean('is_completed')->default(false);
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
             $table->timestamps();
+
+            $table->foreignId('list_id')->constrained('task_lists', 'id')->onDelete('cascade');
         });
     }
 
