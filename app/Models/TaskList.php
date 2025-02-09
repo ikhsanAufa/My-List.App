95f47146a3a79e8fbc8697698aee984b923eaa2a
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-09 22:44:12 +0700

    buat schema model tasklist

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskList extends Model
 {
-    //
+    protected $fillable = ['name'];
+    protected $guarded = [
+        'id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function tasks() {
+        return $this->hasMany(Task::class, 'list_id');
+    }
 }
