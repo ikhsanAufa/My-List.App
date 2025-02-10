99a73544c0220b0b77144105f03a44d06a8c6c03
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:12:41 +0700

    Membuat input name list pada modal

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -8,6 +8,13 @@
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
                     aria-label="Close"></button>
             </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama List</label>
+                    <input type="text" class="form-control bg-secondary text-light" id="name" name="name"
+                        placeholder="Masukkan nama list">
+                </div>
+            </div>
         </form>
     </div>
 </div>
