1fb917f77add86d16cc3950b0fbe29ff68cc517a
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:49:26 +0700

    Menambahkan route get untuk task di routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TaskController::class, 'index'])->name('home');
 
 Route::resource('lists', TaskListController::class);
+
+Route::resource('tasks', TaskController::class);
