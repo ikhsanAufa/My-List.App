a7f32326b38471a8062f8b5304198a4f921415bb
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 19:25:23 +0700

    Membuat fungsi schema drop if exist task di migration

--- database/migrations/2025_02_09_152830_create_tasks_table.php
+++ database/migrations/2025_02_09_152830_create_tasks_table.php
@@ -23,4 +23,11 @@ return new class extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
 };
