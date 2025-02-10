9ca8398edaf7eaad45c83629db4495496f32de4c
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:16:11 +0700

    Membuat modal edit task

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -22,3 +22,56 @@
         </form>
     </div>
 </div>
+
+<div class="modal fade" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form action="{{ route('tasks.store') }}" method="POST" class="modal-content bg-secondary text-light">
+            @method('POST')
+            @csrf
+            <div class="modal-header border-0">
+                <h1 class="modal-title fs-5" id="addTaskModalLabel">Tambah Task</h1>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="text" id="taskListId" name="list_id" hidden>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama Task</label>
+                    <input type="text" class="form-control bg-dark text-light" id="name" name="name"
+                        placeholder="Masukkan nama task">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Deskripsi</label>
+                    <input type="text" class="form-control bg-dark text-light" id="description" name="description"
+                        placeholder="Masukkan deskripsi">
+                </div>
+                <div class="mb-4">
+                    <label for="color" class="form-label">Pilih Prioritas:</label>
+                    <select id="priority" name="priority" class="form-control bg-dark text-light">
+                        <option value="">-</option>
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer border-0">
+                <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Batal</button>
+                <button type="submit" class="btn btn-outline-primary">Tambah</button>
+            </div>
+        </form>
+    </div>
+</div>
+<div class="modal fade" id="editTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form action="{{ route('tasks.store') }}" method="POST" class="modal-content bg-secondary text-light">
+            @method('POST')
+            @csrf
+            <div class="modal-header border-0">
+                <h1 class="modal-title fs-5" id="addTaskModalLabel">Edit Task</h1>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+        </form>
+    </div>
+</div>
