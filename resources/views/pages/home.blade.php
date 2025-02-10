7bb419c36f8b53e670c97d1798d4327910950afc
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 19:44:54 +0700

    Membuat content Total list

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,1 +1,17 @@
+@extends('layouts.app')
 
+@section('content')
+    <!-- Sale & Revenue Start -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-data fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-1">Total Lists</p>
+                        <h6 class="mb-0 text-center">{{ $lists->count() }}</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
