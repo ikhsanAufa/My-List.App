e79a07a0444248412f5b6eae820663325d1ba1c3
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:47:44 +0700

    Menambahkan route untuk home di routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Membuat route untuk home
+Route::get('/', [TaskController::class, 'index'])->name('home');
