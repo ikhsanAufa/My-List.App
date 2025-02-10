364a947d5f60a2aaab0ef3044696a24fb63bc4bd
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:24:47 +0700

    Membuat box search

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,6 +17,9 @@
         <a href="#" class="sidebar-toggler flex-shrink-0">
             <i class="fa fa-bars"></i>
         </a>
+        <form class="d-none d-md-flex ms-4">
+            <input class="form-control bg-dark border-0" type="search" placeholder="Search" />
+        </form>
     </nav>
     <!-- Navbar End -->
 
