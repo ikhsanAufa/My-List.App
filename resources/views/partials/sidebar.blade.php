3d8ba7fc21c1f8ea077a51ad41c084b43fc51a56
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:42:48 +0700

    Menambahkan foto admin

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -6,6 +6,16 @@
                 <i class="fa fa-user-edit me-2"></i>My List
             </h3>
         </a>
+        <div class="d-flex align-items-center ms-4 mb-4">
+            <div class="position-relative">
+                <img class="rounded-circle" src="{{ asset('assets/img/admin.jpeg') }}" alt=""
+                    style="width: 40px; height: 40px" />
+            </div>
+            <div class="ms-3">
+                <h6 class="mb-0">Ikhsan Aufa</h6>
+                <span>Admin</span>
+            </div>
+        </div>
     </nav>
 </div>
 <!-- Sidebar End -->
