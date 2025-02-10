117275e174abda4d780d738d51cd696b890c2990
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 10:36:31 +0700

    Membuat controlle validate pada tasklist

---
+++ app/Http/Controllers/TaskListController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TaskList;
+use Illuminate\Http\Request;
+
+class TaskListController extends Controller
+{
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|max:100'
+        ]);
+
+        return redirect()->back();
+    }
+}
