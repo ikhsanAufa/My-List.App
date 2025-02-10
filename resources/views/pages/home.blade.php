7a1d9494c65413a509ec0829194feea3a644593b
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 19:48:28 +0700

    Membuat content Task completed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -22,5 +22,14 @@
                     </div>
                 </div>
             </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-check fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Task Completed</p>
+                        <h6 class="mb-0 text-center">{{ $tasks->where('is_completed', true)->count() }}</h6>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
