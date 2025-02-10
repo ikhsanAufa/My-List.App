987f5211ad28bd1bb2e3d66e0f7eee5745f8b6a4
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-10 20:43:33 +0700

    Menambahkan border circle untuk foto admin

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -10,6 +10,9 @@
             <div class="position-relative">
                 <img class="rounded-circle" src="{{ asset('assets/img/admin.jpeg') }}" alt=""
                     style="width: 40px; height: 40px" />
+                <div
+                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                </div>
             </div>
             <div class="ms-3">
                 <h6 class="mb-0">Ikhsan Aufa</h6>
