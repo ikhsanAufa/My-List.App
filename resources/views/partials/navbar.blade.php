d6f5509c67a94e6ed8dc718c6d339b0c5c1a52ca
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-13 22:18:33 +0700

    Membuat animasi berkedip skala membesar sedikit indikator merah

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -37,6 +37,11 @@
                 transform: translate(50%, -50%) scale(1);
                 /* Skala normal */
             }
+
+            50% {
+                transform: translate(50%, -50%) scale(1.2);
+                /* Membesar sedikit */
+            }
         }
     </style>
 
