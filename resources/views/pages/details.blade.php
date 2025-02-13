3fa3143ac9070ef1680621f1824f54a0055a347e
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-13 20:43:11 +0700

    Membuat input nama

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -19,6 +19,14 @@
                     <h1 class="modal-title fs-5" id="editTaskModalLabel">Edit Task</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <div class="modal-body bg-secondary">
+                    <input type="text" name="list_id" hidden>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" name="name"
+                            placeholder="Masukkan nama list">
+                    </div>
+                </div>
             </form>
         </div>
     </div>
