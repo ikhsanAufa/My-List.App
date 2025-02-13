c76018bf286280b0ec5dab3e2c395570d788bb7b
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-13 20:33:25 +0700

    Membuat modal edit task

--- resources/views/pages/details.blade.php
+++ resources/views/pages/details.blade.php
@@ -9,4 +9,16 @@
             </a>
         </div>
     </div>
+
+    <div class="modal fade" id="editTaskModal" tabindex="-1" aria-labelledby="editTaskModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <form action="{{ route('tasks.update', $task->id) }}" method="POST" class="modal-content">
+                @method('PUT')
+                @csrf
+                <div class="modal-header bg-dark">
+                    <h1 class="modal-title fs-5" id="editTaskModalLabel">Edit Task</h1>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
