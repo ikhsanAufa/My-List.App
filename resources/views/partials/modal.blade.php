0a5d14a5a12404c7bebd6e43f660b47a255cb693
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-13 21:43:27 +0700

    membuat modal edit

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -1,64 +1,133 @@
-<div class="modal fade" id="addListModal" tabindex="-1" aria-labelledby="addListModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <form action="{{ route('lists.store') }}" method="POST" class="modal-content bg-dark text-light">
-            @method('POST')
-            @csrf
-            <div class="modal-header border-0">
-                <h1 class="modal-title fs-5" id="addListModalLabel">Tambah List</h1>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
-                    aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nama List</label>
-                    <input type="text" class="form-control bg-secondary text-light" id="name" name="name"
-                        placeholder="Masukkan nama list">
-                </div>
-            </div>
-            <div class="modal-footer border-0">
-                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-outline-primary">Tambah</button>
-            </div>
-        </form>
-    </div>
-</div>
+@extends('layouts.app')
 
-<div class="modal fade" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <form action="{{ route('tasks.store') }}" method="POST" class="modal-content bg-secondary text-light">
-            @method('POST')
-            @csrf
-            <div class="modal-header border-0">
-                <h1 class="modal-title fs-5" id="addTaskModalLabel">Tambah Task</h1>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal"
-                    aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <input type="text" id="taskListId" name="list_id" hidden>
-                <div class="mb-3">
-                    <label for="name" class="form-label">Nama Task</label>
-                    <input type="text" class="form-control bg-dark text-light" id="name" name="name"
-                        placeholder="Masukkan nama task">
-                </div>
-                <div class="mb-3">
-                    <label for="description" class="form-label">Deskripsi</label>
-                    <input type="text" class="form-control bg-dark text-light" id="description" name="description"
-                        placeholder="Masukkan deskripsi">
-                </div>
-                <div class="mb-4">
-                    <label for="color" class="form-label">Pilih Prioritas:</label>
-                    <select id="priority" name="priority" class="form-control bg-dark text-light">
-                        <option value="">-</option>
-                        <option value="low">Low</option>
-                        <option value="medium">Medium</option>
-                        <option value="high">High</option>
-                    </select>
+@section('content')
+    <!-- Sale & Revenue Start -->
+    <div class="container-fluid pt-4 px-4">
+        <div class="row g-4">
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-data fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-1">Total Lists</p>
+                        <h6 class="mb-0 text-center">{{ $lists->count() }}</h6>
+                    </div>
                 </div>
             </div>
-            <div class="modal-footer border-0">
-                <button type="button" class="btn btn-outline-dark" data-bs-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-outline-primary">Tambah</button>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-heart fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Total Tasks</p>
+                        <h6 class="mb-0 text-center">{{ $tasks->count() }}</h6>
+                    </div>
+                </div>
             </div>
-        </form>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-check fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Task Completed</p>
+                        <h6 class="mb-0 text-center">{{ $tasks->where('is_completed', true)->count() }}</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="bg-secondary rounded d-flex align-items-center justify-content-center p-4">
+                    <i class="bi bi-clipboard-x fs-1 text-primary"></i>
+                    <div class="ms-3">
+                        <p class="mb-2">Not Completed</p>
+                        <h6 class="mb-0 text-center">{{ $tasks->where('is_completed', false)->count() }}</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    <!-- Sale & Revenue End -->
+
+    <div class="container-fluid pt-4 px-4 overflow-y-hidden">
+        <div class="row">
+            @if ($lists->count() == 0)
+                <p class="text-center">Please add your assignment</p>
+            @endif
+
+            <div class="d-flex gap-3 flex-nowrap overflow-y-scroll" style="height: 100vh;">
+                @foreach ($lists as $list)
+                    <div class="card overflow-auto bg-secondary flex-shrink-0" style="width: 18rem; max-height: 95vh;">
+                        <div
+                            class="card-header overflow-y-hidden bg-primary d-flex align-items-center justify-content-between">
+                            <h4 class="card-title">{{ $list->name }}</h4>
+                            <form action="{{ route('lists.destroy', $list->id) }}" method="POST" style="display: inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm p-0">
+                                    <i class="bi bi-trash fs-5 text-dark"></i>
+                                </button>
+                            </form>
+                        </div>
+                        <div class="card-body d-flex flex-column gap-2 overflow-x-hidden">
+                            @foreach ($tasks as $task)
+                                @if ($task->list_id == $list->id)
+                                    <div class="card bg-dark my-2">
+                                        <div class="card-header pt-3">
+                                            <div class="d-flex align-items-center justify-content-between">
+                                                <div class="d-flex flex-column justify-content-center gap-2">
+                                                    <a href="{{ route('tasks.show', $task->id) }}"
+                                                        class="fw-bold lh-1 m-0 text-decoration-none text-{{ $task->priorityClass }} {{ $task->is_completed ? 'text-decoration-line-through' : '' }}">
+                                                        {{ $task->name }}
+                                                    </a>
+                                                    <span class="badge text-bg-{{ $task->priorityClass }} badge-pill"
+                                                        style="width: fit-content">
+                                                        {{ $task->priority }}
+                                                    </span>
+                                                </div>
+                                                <form action="{{ route('tasks.destroy', $task->id) }}" method="POST"
+                                                    style="display: inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm p-0">
+                                                        <i class="bi bi-x-circle text-danger fs-5"></i>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="card-body bg-secondary">
+                                            <p class="card-text text-truncate">
+                                                {{ $task->description }}
+                                            </p>
+                                        </div>
+                                        @if (!$task->is_completed)
+                                            <form action="{{ route('tasks.complete', $task->id) }}" method="POST">
+                                                @csrf
+                                                @method('PATCH')
+                                                <button type="submit" class="btn btn-sm btn-outline-success w-100">
+                                                    <i class="bi bi-check2-circle"></i>
+                                                    Selesai
+                                                </button>
+                                            </form>
+                                        @endif
+                                    </div>
+                                @endif
+                            @endforeach
+                            <button type="button" class="btn btn-sm btn-outline-primary" data-bs-toggle="modal"
+                                data-bs-target="#addTaskModal" data-list="{{ $list->id }}">
+                                <span class="d-flex align-items-center justify-content-center">
+                                    <i class="bi bi-plus fs-5"></i>
+                                    Tambah
+                                </span>
+                            </button>
+                        </div>
+                        <div class="card-footer bg-primary text-dark d-flex justify-content-between align-items-center">
+                            <p class="card-text">{{ $list->tasks->count() }} Tugas</p>
+                        </div>
+                    </div>
+                @endforeach
+                <button class="btn btn-outline-primary flex-shrink-0" style="height: fit-content;" data-bs-toggle="modal"
+                    data-bs-target="#addListModal">
+                    <span class="d-flex align-items-center justify-content-center">
+                        <i class="bi bi-plus fs-5"></i>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </div>
+@endsection
