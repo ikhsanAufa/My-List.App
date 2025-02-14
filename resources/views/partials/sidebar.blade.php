51d0f5f3646d9f75074650f4f078292a496cf121
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-14 10:04:32 +0700

    Menambahkan icon instagram di sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,27 +1,35 @@
-<!-- Sidebar Start -->
-<div class="sidebar pe-4 pb-3">
-    <nav class="navbar bg-secondary navbar-dark">
-        <a href="index.html" class="navbar-brand mx-4 mb-3">
-            <h3 class="text-primary">
-                <i class="fa fa-user-edit me-2"></i>My List
-            </h3>
-        </a>
-        <div class="d-flex align-items-center ms-4 mb-4">
-            <div class="position-relative">
-                <img class="rounded-circle" src="{{ asset('assets/img/admin.jpeg') }}" alt=""
-                    style="width: 40px; height: 40px" />
-                <div
-                    class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+    <!-- Sidebar Start -->
+    <div class="sidebar pe-4 pb-3">
+        <nav class="navbar bg-secondary navbar-dark">
+            <a href="index.html" class="navbar-brand mx-4 mb-3">
+                <h3 class="text-primary">
+                    <i class="bi bi-emoji-wink"></i>
+                    My List
+                </h3>
+            </a>
+            <div class="d-flex align-items-center ms-4 mb-4">
+                <div class="position-relative">
+                    <img class="rounded-circle" src="{{ asset('assets/img/admin.jpeg') }}" alt=""
+                        style="width: 40px; height: 40px" />
+                    <div
+                        class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1">
+                    </div>
+                </div>
+                <div class="ms-3">
+                    <h6 class="mb-0">Ikhsan Aufa</h6>
+                    <span>Admin</span>
                 </div>
             </div>
-            <div class="ms-3">
-                <h6 class="mb-0">Ikhsan Aufa</h6>
-                <span>Admin</span>
+            <div class="navbar-nav w-100">
+                <a href="index.html" class="nav-item nav-link active"><i
+                        class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
             </div>
-        </div>
-        <div class="navbar-nav w-100">
-            <a href="index.html" class="nav-item nav-link active"><i class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
-        </div>
-    </nav>
-</div>
-<!-- Sidebar End -->
+            <div class="navbar-nav w-200">
+                <span class="fs-2 my-3">
+                    <a href="https://www.instagram.com/icaannzz/?igsh=bndqYXRob3ozdGI4#"><i
+                            class="bi bi-instagram mx-4 text-primary"></i></a>
+                </span>
+            </div>
+        </nav>
+    </div>
+    <!-- Sidebar End -->
