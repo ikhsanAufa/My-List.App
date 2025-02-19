7bd4c767e6d45867ea2e53f61b3b1d535c932eed
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-19 09:54:22 +0700

    Menambahkan komentar di Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,9 @@ use App\Models\TaskList;
 
 class Task extends Model
 {
-    protected $fillable = [
+    protected $fillable 
+    // protected $fillable di Laravel digunakan untuk menentukan kolom mana yang diizinkan untuk diisi secara massal pada model Eloquent.
+    = [
         'name',
         'description',
         'is_completed',
@@ -15,7 +17,9 @@ class Task extends Model
         'list_id'
     ];
 
-    protected $guarded = [
+    protected $guarded 
+    // protected $guarded di Laravel adalah properti yang digunakan untuk menentukan kolom yang tidak boleh diisi melalui mass assignment pada model Eloquent.
+    = [
         'id',
         'created_at',
         'updated_at'
@@ -27,7 +31,9 @@ class Task extends Model
         'high'
     ];
 
-    public function getPriorityClassAttribute() {
+    public function getPriorityClassAttribute()
+    // getPriorityClassAttribute() adalah sebuah Accessor di Laravel yang memungkinkan Anda untuk memodifikasi atau memformat nilai atribut priority_class model secara otomatis saat diakses.
+    {
         return match($this->attributes['priority']) {
             'low' => 'success',
             'medium' => 'warning',
