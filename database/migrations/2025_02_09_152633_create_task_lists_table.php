f64ca8f9bfe5049520c591694b22efef9e4c101d
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-20 10:32:28 +0700

    Menambahkan komentar di tasklist table

--- database/migrations/2025_02_09_152633_create_task_lists_table.php
+++ database/migrations/2025_02_09_152633_create_task_lists_table.php
@@ -1,8 +1,14 @@
 <?php
+// Tasklist table di Laravel adalah sebuah tabel database yang digunakan untuk menyimpan data tugas atau daftar pekerjaan yang perlu dikelola dalam aplikasi.
 
 use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Migrations\Migration; adalah perintah use yang digunakan untuk mengimpor kelas Migration dari namespace Illuminate\Database\Migrations.
+
 use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Database\Schema\Blueprint; adalah perintah untuk mengimpor kelas Blueprint di Laravel, yang digunakan untuk mendefinisikan struktur tabel di database saat membuat atau mengubah tabel dalam file migrasi.
+
 use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema; adalah cara untuk mengimpor Facade Schema di Laravel, yang digunakan untuk mempermudah manipulasi struktur database, seperti membuat tabel baru, menambah atau menghapus kolom, dan menghapus tabel.
 
 return new class extends Migration
 {
