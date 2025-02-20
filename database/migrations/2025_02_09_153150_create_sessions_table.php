6b752241660a0c777748317fe5c24ae2f4453c9d
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-20 10:11:35 +0700

    Menambahkan komentar di session table

--- database/migrations/2025_02_09_153150_create_sessions_table.php
+++ database/migrations/2025_02_09_153150_create_sessions_table.php
@@ -8,13 +8,19 @@ use Illuminate\Database\Schema\Blueprint;
 // use Illuminate\Database\Schema\Blueprint; adalah perintah untuk mengimpor kelas Blueprint di Laravel, yang digunakan untuk mendefinisikan struktur tabel di database saat membuat atau mengubah tabel dalam file migrasi.
 
 use Illuminate\Support\Facades\Schema;
+// use Illuminate\Support\Facades\Schema; adalah cara untuk mengimpor Facade Schema di Laravel, yang digunakan untuk mempermudah manipulasi struktur database, seperti membuat tabel baru, menambah atau menghapus kolom, dan menghapus tabel.
 
 return new class extends Migration
+// return new class extends Migration adalah sintaks untuk membuat kelas anonim yang mewarisi kelas Migration di Laravel. 
 {
     /**
      * Run the migrations.
      */
     public function up(): void
+    // public function up(): void adalah metode dalam migrasi Laravel yang digunakan untuk mendefinisikan perubahan pada struktur database (misalnya membuat atau mengubah tabel).
+    // : void menunjukkan bahwa metode ini tidak akan mengembalikan nilai apapun.
+    // Metode up() berfungsi untuk mendefinisikan dan menerapkan perubahan pada database, dan ketika migrasi dibatalkan
+
     {
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
@@ -30,6 +36,9 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
+    // public function down(): void adalah metode dalam kelas migrasi Laravel yang digunakan untuk membatalkan perubahan yang dilakukan oleh up(). Misalnya, menghapus tabel atau kolom yang telah ditambahkan dalam up().
+    // : void menunjukkan bahwa down() tidak mengembalikan nilai apapun.
+    // Metode down() penting untuk mendukung rollback migrasi, memungkinkan kita untuk kembali ke struktur database sebelumnya jika diperlukan.
     {
         Schema::dropIfExists('sessions');
     }
