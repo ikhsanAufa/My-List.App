278284e22528339458d3c133fbbb1831b7137aec
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-20 11:14:11 +0700

    Menambahkan komentar di tasklist seeder

--- database/seeders/TaskListSeeder.php
+++ database/seeders/TaskListSeeder.php
@@ -1,8 +1,11 @@
 <?php
+// TaskListSeeder di Laravel adalah sebuah seeder class yang digunakan untuk mengisi tabel task_lists di database dengan data sementara.
 
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// adalah pernyataan use dalam file seeder Laravel yang digunakan untuk menonaktifkan event model selama proses seeding data ke dalam database.
+
 use Illuminate\Database\Seeder;
 use App\Models\TaskList;
 
