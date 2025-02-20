eb6960be368394fd4ba8471b8b167c6c070f9857
IkhsanAufa <ikhsanaufarahman@gmail.com>
2025-02-20 09:02:08 +0700

    Menambahkan komentar di TaskList.php

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -6,8 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskList extends Model
 {
-    protected $fillable = ['name'];
-    protected $guarded = [
+    protected $fillable = 
+    // protected $fillable di Laravel digunakan untuk menentukan kolom mana yang diizinkan untuk diisi secara massal pada model Eloquent.
+    ['name'];
+    protected $guarded =
+    // protected $guarded di Laravel adalah properti yang digunakan untuk menentukan kolom yang tidak boleh diisi melalui mass assignment pada model Eloquent.
+    [
         'id',
         'created_at',
         'updated_at'
